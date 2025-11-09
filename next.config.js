6476f2e0534609081e2664103e27e777d39c00a5
pousadadocas-io <docaspousada@gmail.com>
2025-11-09 17:47:46 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
-const basePath = isProd ? '/docas-front' : '';
+const basePath = isProd ? '/docas-app' : '';
 
 const nextConfig = {
   output: 'export',
